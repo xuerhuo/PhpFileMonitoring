c7b03c28403a76f63a163918d86d2337f2a72276
xuerhuo <824851582@qq.com>
2017-08-24 12:39:11 +0800

    修改存储方式未序列化 增加检测删除

--- monitoring.php
+++ monitoring.php
@@ -5,28 +5,31 @@
  * Date: 2017/8/14
  * Time: 21:03
  */
-echo "\r\n=========================扫描时间".date('Y-m-d H:i:s')."==============================\r\n";
+echo "\r\n=========================start time".date('Y-m-d H:i:s')."==============================\r\n";
+error_reporting(7);
 $savepath='/root/';
-$path = '/var/www/zxwk/public_html/';
-$pre_data = json_decode(file_get_contents($savepath.'scan.dat'),1);
+$path = '/www/web/www/public_html/';
+$pre_data = unserialize(file_get_contents($savepath.'scan.dat'));
 $files = scanpath($path);
 foreach ($files as $file){
-    $data[$file] = md5_file($file);
+    $temp = md5_file($file);
+    if($temp)
+        $data[$file] = $temp;
 }
 foreach($data as $key=>$value){
     if(empty($pre_data[$key])){
-        echo date("Y-m-d H:i:s",time())."创建文件:".$key."\r\n";
+        echo date("Y-m-d H:i:s",time())."create:".$key."\r\n";
         continue;
     }elseif($value!=$pre_data[$key]){
-        echo date("Y-m-d H:i:s",time())."修改文件:".$key."\r\n";
+        echo date("Y-m-d H:i:s",time())."modify:".$key."\r\n";
     }
 }
 foreach($pre_data as $key=>$value){
     if(empty($data[$key])){
-        echo date("Y-m-d H:i:s",time())."删除文件:".$key."\r\n";
+        echo date("Y-m-d H:i:s",time())."deleteed:".$key."\r\n";
     }
 }
-file_put_contents($savepath.'scan.dat',json_encode($data));
+file_put_contents($savepath.'scan.dat',serialize($data));
 function scanpath($path, $except = null, &$data = null)
 {
     $except = is_array($except) ? $except : array($except);
