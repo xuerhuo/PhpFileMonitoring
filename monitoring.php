56f487faefe9368bf07520385b4c9ff99d8f6256
erhuo <i@erhuo.org>
2018-12-08 20:52:08 +0800

    .

--- monitoring.php
+++ monitoring.php
@@ -7,43 +7,58 @@
  */
 echo "\r\n=========================start time".date('Y-m-d H:i:s')."==============================\r\n";
 error_reporting(7);
-$savepath='/root/';
-$path = '/www/web/www/public_html/';
-$except = ['js','css','jpg','jpeg','zip','png','gif','html','htm'];
-$print_tips = true;
-$pre_data = unserialize(file_get_contents($savepath.'scan.dat'));
+$option['savepath'] = dirname(__FILE__);
+$path = ['/www/web/xishui/public_html/'];
+$except = ['js','css','jpg','jpeg','zip','png','gif','html','htm','svg','_addons_'];
+checkFiles($path,$except,$option);
 
 
-$files = scanpath($path,$except);
-if($print_tips){
-    echo "end scan.".count($files)." files.\r\n";
+
+function checkFiles($paths,$except,$option){
+    $print_tips = true;
+    $files = [];
+    $pre_data = unserialize(file_get_contents($option['savepath'].'scan.dat'));
+    foreach ($paths as $path){
+        $files = array_merge($files,scanpath($path,$except));
+        if($print_tips){
+            echo "end scan.".count($files)." files.\r\n";
+        }
+    }
+    foreach ($files as $file){
+        $temp = md5_file($file);
+        if($temp)
+            $data[$file] = $temp;
+        if($print_tips && count($data)%1000==0){
+            echo count($data)." files has computed hash. end file is ".$file."\r\n";
+        }
+    }
+    if($print_tips){
+        echo "\r\n end md5files.".count($data)." files.\r\n";
+    }
+    foreach($data as $key=>$value){
+        if(empty($pre_data[$key])){
+            echo date("Y-m-d H:i:s",time())."create:".$key."\r\n";
+            continue;
+        }elseif($value!=$pre_data[$key]){
+            echo date("Y-m-d H:i:s",time())."modify:".$key."\r\n";
+        }
+    }
+    foreach($pre_data as $key=>$value){
+        if(empty($data[$key])){
+            echo date("Y-m-d H:i:s",time())."deleteed:".$key."\r\n";
+        }
+    }
+    fwrite ( STDOUT , 'save the hash data：y/n' . PHP_EOL );
+    $input = trim(fgets(STDIN));
+    if($input=='y') {
+        file_put_contents($option['savepath'] . 'scan.dat', serialize($data));
+        fputs("hash saved.");
+    }else{
+        fputs("hash file not save");
+    }
 }
 
-foreach ($files as $file){
-    $temp = md5_file($file);
-    if($temp)
-        $data[$file] = $temp;
-    if($print_tips && count($data)%1000==0){
-        echo count($data)." files has computed hash. end file is ".$file."\r\n";
-    }
-}
-if($print_tips){
-    echo "\r\n end md5files.".count($data)." files.\r\n";
-}
-foreach($data as $key=>$value){
-    if(empty($pre_data[$key])){
-        echo date("Y-m-d H:i:s",time())."create:".$key."\r\n";
-        continue;
-    }elseif($value!=$pre_data[$key]){
-        echo date("Y-m-d H:i:s",time())."modify:".$key."\r\n";
-    }
-}
-foreach($pre_data as $key=>$value){
-    if(empty($data[$key])){
-        echo date("Y-m-d H:i:s",time())."deleteed:".$key."\r\n";
-    }
-}
-file_put_contents($savepath.'scan.dat',serialize($data));
+
 function scanpath($path, $except = null, &$data = null)
 {
     $except = is_array($except) ? $except : array($except);
